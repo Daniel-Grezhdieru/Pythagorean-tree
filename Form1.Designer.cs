1b3226a6029f422f6b3fa54f075ca396db3e2a02
Daniel-Grezhdieru <grejdieru@yandex.ru>
2019-09-18 21:27:34 +0300

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,311 @@
+﻿namespace Pythagorean_tree
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.nudAlpha = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.nudLength = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.nudDepth = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Panel_paint = new System.Windows.Forms.PictureBox();
+            this.chkFill = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.auto = new System.Windows.Forms.CheckBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.nudAlpha)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudLength)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudDepth)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Panel_paint)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // nudAlpha
+            // 
+            this.nudAlpha.Location = new System.Drawing.Point(97, 128);
+            this.nudAlpha.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.nudAlpha.Maximum = new decimal(new int[] {
+            360,
+            0,
+            0,
+            0});
+            this.nudAlpha.Name = "nudAlpha";
+            this.nudAlpha.Size = new System.Drawing.Size(63, 26);
+            this.nudAlpha.TabIndex = 17;
+            this.nudAlpha.Value = new decimal(new int[] {
+            45,
+            0,
+            0,
+            0});
+            this.nudAlpha.ValueChanged += new System.EventHandler(this.parameter_ValueChanged);
+            this.nudAlpha.Click += new System.EventHandler(this.nudAlpha_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(18, 128);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 20);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Угол:";
+            // 
+            // nudLength
+            // 
+            this.nudLength.Increment = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.nudLength.Location = new System.Drawing.Point(97, 75);
+            this.nudLength.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.nudLength.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nudLength.Name = "nudLength";
+            this.nudLength.Size = new System.Drawing.Size(63, 26);
+            this.nudLength.TabIndex = 13;
+            this.nudLength.Value = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            this.nudLength.ValueChanged += new System.EventHandler(this.parameter_ValueChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 75);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 20);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Длина:";
+            // 
+            // nudDepth
+            // 
+            this.nudDepth.Location = new System.Drawing.Point(97, 24);
+            this.nudDepth.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.nudDepth.Maximum = new decimal(new int[] {
+            15,
+            0,
+            0,
+            0});
+            this.nudDepth.Name = "nudDepth";
+            this.nudDepth.Size = new System.Drawing.Size(63, 26);
+            this.nudDepth.TabIndex = 11;
+            this.nudDepth.Value = new decimal(new int[] {
+            4,
+            0,
+            0,
+            0});
+            this.nudDepth.ValueChanged += new System.EventHandler(this.parameter_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 26);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 20);
+            this.label1.TabIndex = 10;
+            this.label1.Text = " Шаги:";
+            // 
+            // Panel_paint
+            // 
+            this.Panel_paint.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Panel_paint.BackColor = System.Drawing.SystemColors.Info;
+            this.Panel_paint.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.Panel_paint.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.Panel_paint.Location = new System.Drawing.Point(183, 14);
+            this.Panel_paint.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Panel_paint.Name = "Panel_paint";
+            this.Panel_paint.Size = new System.Drawing.Size(818, 595);
+            this.Panel_paint.TabIndex = 9;
+            this.Panel_paint.TabStop = false;
+            this.Panel_paint.Paint += new System.Windows.Forms.PaintEventHandler(this.Panel_Paint);
+            this.Panel_paint.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pic_down);
+            this.Panel_paint.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pic_move);
+            this.Panel_paint.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pic_up);
+            // 
+            // chkFill
+            // 
+            this.chkFill.CheckAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.chkFill.Location = new System.Drawing.Point(22, 286);
+            this.chkFill.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.chkFill.Name = "chkFill";
+            this.chkFill.Size = new System.Drawing.Size(138, 37);
+            this.chkFill.TabIndex = 18;
+            this.chkFill.Text = "Раскрасить:";
+            this.chkFill.UseVisualStyleBackColor = true;
+            this.chkFill.CheckedChanged += new System.EventHandler(this.parameter_ValueChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(22, 361);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(138, 41);
+            this.button1.TabIndex = 19;
+            this.button1.Text = "Сохранить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(97, 178);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            500,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            500,
+            0,
+            0,
+            -2147483648});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(63, 26);
+            this.numericUpDown1.TabIndex = 20;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(97, 231);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            500,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Minimum = new decimal(new int[] {
+            500,
+            0,
+            0,
+            -2147483648});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(63, 26);
+            this.numericUpDown2.TabIndex = 21;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 178);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 20);
+            this.label3.TabIndex = 22;
+            this.label3.Text = "Ox+-";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(18, 231);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(42, 20);
+            this.label5.TabIndex = 23;
+            this.label5.Text = "Oy+-";
+            // 
+            // auto
+            // 
+            this.auto.AutoSize = true;
+            this.auto.CheckAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.auto.Location = new System.Drawing.Point(22, 585);
+            this.auto.Name = "auto";
+            this.auto.Size = new System.Drawing.Size(113, 24);
+            this.auto.TabIndex = 24;
+            this.auto.Text = "авто шаги";
+            this.auto.UseVisualStyleBackColor = true;
+            this.auto.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(1014, 623);
+            this.Controls.Add(this.auto);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chkFill);
+            this.Controls.Add(this.nudAlpha);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.nudLength);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.nudDepth);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Panel_paint);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Pythagorean_tree";
+            ((System.ComponentModel.ISupportInitialize)(this.nudAlpha)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudLength)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudDepth)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Panel_paint)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown nudAlpha;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown nudLength;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown nudDepth;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox Panel_paint;
+        private System.Windows.Forms.CheckBox chkFill;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.CheckBox auto;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+
+using System.Drawing.Drawing2D;
+
+namespace Pythagorean_tree
+{
+    public partial class Form1 : Form
+    {
+        bool movable;
+        float mouse_x;
+        float mouse_y;
+        float mouse_x_offset;
+        float  mouse_y_offset;
+        Graphics gr;
+        Bitmap image;
+        float root_x;
+        float root_y;
+        int depth;
+        int length;
+        float alpha;
+        public Form1()
+        {
+            InitializeComponent();
+            Init();
+            
+        }
+
+        public void Init()
+        {
+            image = new Bitmap(Panel_paint.Width, Panel_paint.Height); // создаем растровое изображение
+            gr = Graphics.FromImage(image);
+            gr.Clear(Color.White);
+            Panel_paint.Image = image;
+            gr.SmoothingMode = SmoothingMode.AntiAlias;
+        }
+
+        // Рисуем дерево.
+        private void Panel_Paint(object sender, PaintEventArgs e)
+        {
+          
+            gr.Clear(Panel_paint.BackColor);
+
+            try
+            {
+                depth = (int)nudDepth.Value;
+                length = (int)nudLength.Value;
+                if (auto.Checked)
+                {
+                    depth = length / 10 + 4;
+                }
+                alpha = (float)((double)nudAlpha.Value * Math.PI / 180.0);
+                root_x = Panel_paint.ClientSize.Width / 2 + (float)numericUpDown1.Value * 10 + mouse_x_offset;
+                root_y = Panel_paint.ClientSize.Height * 0.95f + (float)numericUpDown2.Value * 10 + mouse_y_offset;
+                VectorF v_base = new VectorF(length, 0);
+                PointF ll_corner = new PointF(root_x, root_y) - v_base / 2;
+
+                Brush brush = null;
+                Brush brush1 = null;
+                if (chkFill.Checked)
+                {
+                    brush = Brushes.Green;
+                    brush1 = Brushes.Brown;
+                }
+               
+
+                DrawBranch(gr, Pens.Black, brush,brush1,
+                    depth, ll_corner, v_base, alpha);
+            }
+            catch
+            {
+
+            }
+        }
+
+        // Перерисовка.
+        private void parameter_ValueChanged(object sender, EventArgs e)
+        {
+            //Panel_paint.Refresh();
+
+        }
+        //private void Panel_Resize(object sender, EventArgs e)
+        //{
+        //    Panel_paint.Refresh();
+        //}
+        private void nud_KeyUp(object sender, KeyEventArgs e)
+        {
+            //Panel_paint.Refresh();
+        }
+
+        // Рекурсивная функция отрисовки ветвей.
+        private void DrawBranch(Graphics grf, Pen pen, Brush brush, Brush brush1,
+            int depth, PointF ll_corner, VectorF v_base, float alpha)
+        {
+            // находим координаты точек квадрата.
+            VectorF v_height = v_base.PerpendicularCCW();
+            PointF[] points =
+            {
+                ll_corner,
+                ll_corner + v_base,
+                ll_corner + v_base + v_height,
+                ll_corner + v_height,
+            };
+
+            // Рисуем квадрат.
+            if (brush1 != null) { 
+                gr.FillPolygon(brush1, points);
+                if (v_base.Length<15) gr.FillPolygon(brush, points);
+            }
+            gr.DrawPolygon(pen, points);
+            Panel_paint.Image=image;
+            
+            // если глубина не 0 рисуем ветви.
+            if (depth > 0)
+            {
+                // ***********
+                // Левая ветвь
+                // ***********
+                // новыя длина стороны.
+                double w1 = v_base.Length * Math.Cos(alpha);
+
+                //  координаты нового базового вектора.
+                float wb1 = (float)(w1 * Math.Cos(alpha));
+                float wh1 = (float)(w1 * Math.Sin(alpha));
+                VectorF v_base1 = v_base.Scale(wb1) + v_height.Scale(wh1);
+
+                // нижняя левая координата.
+                PointF ll_corner1 = ll_corner + v_height;
+
+                // отрисовка.
+                DrawBranch(gr, pen, brush,brush1, depth - 1, ll_corner1, v_base1, alpha);
+
+                // ************
+                // Правая ветвь
+                // ************
+                // новая длина.
+                double beta = Math.PI / 2.0 - alpha;
+                double w2 = v_base.Length * Math.Sin(alpha);
+
+                // новый базовый вектор.
+                float wb2 = (float)(w2 * Math.Cos(beta));
+                float wh2 = (float)(w2 * Math.Sin(beta));
+                VectorF v_base2 = v_base.Scale(wb2) - v_height.Scale(wh2);
+
+                // нижняя левая координата.
+                PointF ll_corner2 = ll_corner1 + v_base1;
+
+                // отрисовка.
+                DrawBranch(gr, pen, brush,brush1, depth - 1, ll_corner2, v_base2, alpha);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveDialog = new SaveFileDialog();
+            saveDialog.Title = "Сохранить изображение как...";
+            saveDialog.OverwritePrompt = true; // предупреждение, если пользователь указывает имя уже существующего файла
+            saveDialog.CheckPathExists = true; // предупреждение, если пользователь указывает несуществующий путь
+            saveDialog.Filter = "Image Files(*.BMP)|*.BMP|Image Files(*.JPG)|*.JPG|Image Files(*.GIF)|*.GIF|Image Files(*.PNG)|*.PNG|All files (*.*)|*.*";
+            saveDialog.ShowHelp = true; // кнопка "Справка" в диалоговом окне
+
+            if (saveDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    image.Save(saveDialog.FileName);
+                }
+                catch
+                {
+                    MessageBox.Show("Невозможно сохранить изображение", "Ошибка",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+        //начинаем двигать картинку, нажимая кнопку
+        private void pic_down(object sender, MouseEventArgs e)
+        {
+            movable = true;
+            mouse_x = Cursor.Position.X;
+            mouse_y = Cursor.Position.Y;
+        }
+        //двигаем картинки при нажатой кнопке мыши
+        private void pic_move(object sender, MouseEventArgs e)
+        {
+            if (movable)
+            {
+                mouse_x_offset = Cursor.Position.X - mouse_x;
+                mouse_y_offset = Cursor.Position.Y - mouse_y;
+                //mouse_x = Cursor.Position.X;
+                //mouse_y = Cursor.Position.Y;
+                //Panel_paint.Invalidate();
+
+            }
+        }
+
+        // при отжатии кнопки мыши перестаем перемещать картинку
+        private void pic_up(object sender, MouseEventArgs e)
+        {
+            movable = false;
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            //Panel_paint.Refresh();
+        }
+
+        
+
+        //перерисовыываем изображение
+        //private void repaint_img(object sender, PaintEventArgs e)
+        //{
+        //    e.Graphics.Clear(Color.White);
+        //    e.Graphics.DrawImageUnscaled(image,(int)root_x, (int)root_y);
+        //}
+
+       
+       
+    }
+}
